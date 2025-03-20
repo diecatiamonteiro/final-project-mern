d20db421fa5db1a6fd450af5bdc1f2b2a19f0507
Evie <eviewilcock01@gmail.com>
2025-03-20 16:04:41 +0100

    working on cloudinary

--- client/src/components/UploadImage.jsx
+++ client/src/components/UploadImage.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 export default function UploadImage() {
   const [image, setImage] = useState(null);
 
+  const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+
   const imageHandler = (e) => {
     setImage(e.target.files[0]);
   };
@@ -11,16 +13,14 @@ export default function UploadImage() {
   const uploadImage = async () => {
     const formData = new FormData();
     formData.append("image", image);
+    formData.append("upload_preset", "The Greenroom");
 
     const config = {
-      method: "PATCH",
-      withCredentials: true,
-      headers: {
-        "Content-Type": "multipart/form-data", // If using FormData
-      },
+      method: "POST",
     };
-    const response = await axios.patch(
-      `http://localhost:8000/api/users/67d9787538abedcfa89660f3/update-profile`,
+
+    const response = await axios.post(
+      `https://api.cloudinary.com/v1_1/${cloudName}/upload`,
       formData,
       config
     );
