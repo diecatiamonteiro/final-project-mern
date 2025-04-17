82aad532e46fa049d192d09954eee2067a9fca70
Catia Monteiro <monteiro.cmonteiro@icloud.com>
2025-04-17 11:06:29 +0200

    general setup before deployment

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
