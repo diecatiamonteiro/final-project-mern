0b587fbf3d9c600d38eb7a8f201b43d412079e5e
Catia Monteiro <monteiro.cmonteiro@icloud.com>
2025-04-17 14:20:59 +0200

    added a script to index.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   outDir: "dist",
-  // },
-  // server: {
-  //   port: 5173,
-  // },
-  // publicDir: "public",
 });
