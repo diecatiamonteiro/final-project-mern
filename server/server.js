63e50fbe5ce71218994c37f1284534b0fe6c703f
Evie <eviewilcock01@gmail.com>
2025-04-21 15:47:30 +0200

    added commed for testing purposes. ignore this commit.

--- server/server.js
+++ server/server.js
@@ -43,3 +43,5 @@ app.use(globalErrorHandler);
 // Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
+
+// TEST COMMENT
