4155695fcbe99ef06e3bcaf4c08fc1e8d2bbe956
Evie <eviewilcock01@gmail.com>
2025-04-24 14:23:20 +0200

    test push

--- server/server.js
+++ server/server.js
@@ -60,5 +60,3 @@ app.use(globalErrorHandler);
 // Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
-
-// TEST COMMENT TEST TEST TEST
