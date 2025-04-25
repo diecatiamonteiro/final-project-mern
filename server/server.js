3aa8415cc73ac4ac0a25a671645f27a058b5ea2b
Evie <eviewilcock01@gmail.com>
2025-04-25 17:29:11 +0200

    pushing from main, no new changes

--- server/server.js
+++ server/server.js
@@ -60,3 +60,5 @@ app.use(globalErrorHandler);
 // Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
+
+// comment to remove
